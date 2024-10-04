f1b20d72194628eb65c30f5bdc0d11bb6382dc8a
ghonche <ataee.ghonche@gmail.com>
2024-10-04 14:56:55 +0330

    loginStep1&2

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,39 @@
 
+// import React from 'react';
+// import LeftPanel from '../Screens/SignIn/LeftPanel.jsx';
+// import LoginForm from '../Screens/SignIn/LoginForm.jsx';
+// // import LandingPage from '../Screens/SignIn/LandingPage.jsx';
+
+// const App = () => {
+//   return (
+//     <div className="flex h-screen">
+//       {/* Left Panel */}
+//       <div className="w-1/2 bg-gray-100 p-10 flex items-center">
+//         <LeftPanel />
+//       </div>
+
+//       {/* Right Panel (Login Form) */}
+//       <div className="w-1/2 p-10 flex items-center justify-center">
+//         <LoginForm />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// App.jsx
 import React from 'react';
-import LeftPanel from '../Screens/SignIn/LeftPanel.jsx';
-import LoginForm from '../Screens/SignIn/LoginForm.jsx';
-// import LandingPage from '../Screens/SignIn/LandingPage.jsx';
+import './App.css';
+import { LoginPage } from '../Components/LoginForm';
 
-const App = () => {
+function App() {
   return (
-    <div className="flex h-screen">
-      {/* Left Panel */}
-      <div className="w-1/2 bg-gray-100 p-10 flex items-center">
-        <LeftPanel />
-      </div>
-
-      {/* Right Panel (Login Form) */}
-      <div className="w-1/2 p-10 flex items-center justify-center">
-        <LoginForm />
-      </div>
+    <div className="App">
+      <LoginPage />
     </div>
   );
-};
+}
 
 export default App;
-
