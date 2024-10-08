90169bb9e4163eadba2f54035ae1de03cc509d82
Shayan <nwryshayan500@gmail.com>
2024-10-08 12:31:42 +0330

    Darkmode Completed

--- src/Components/CourseDetailHolder/CourseDesc.jsx
+++ src/Components/CourseDetailHolder/CourseDesc.jsx
@@ -3,25 +3,6 @@ import { Rate } from "antd";
 import React, { useState } from "react";
 import { IoIosLink } from "react-icons/io";
 
-const StarRating = ({ rating }) => {
-  return (
-    <div className="flex items-center mt-4">
-      {[...Array(5)].map((_, index) => (
-        <svg
-          key={index}
-          xmlns="http://www.w3.org/2000/svg"
-          fill={index < rating ? "#FFB700" : "#E4E4E4"}
-          viewBox="0 0 24 24"
-          className="w-6 h-6"
-        >
-          <path d="M12 .587l3.668 7.431 8.24 1.198-5.954 5.426 1.405 8.21L12 18.897l-7.359 3.88 1.405-8.21-5.954-5.426 8.24-1.198L12 .587z" />
-        </svg>
-      ))}
-      <span className="text-[#7C7C7C] ml-2">{rating} از ۵</span>
-    </div>
-  );
-};
-
 const CopyLink = () => {
   const [copied, setCopied] = useState(false);
 
@@ -32,17 +13,34 @@ const CopyLink = () => {
   };
 
   return (
+    // <div className="mr-5 -mt-1">
+    //   <button
+    //     onClick={copyToClipboard}
+    //     className="border-2 border-[#3772FF] text-[#3772FF] text-lg/4 line-heith px-4 py-2 rounded-full"
+    //   >
+    //     {copied ? (
+    //       "کپی شد!"
+    //     ) : (
+    //       <div className="flex">
+    //         <IoIosLink className="-scale-x-[1] ml-2" />
+    //         کپی کردن لینک صفحه
+    //       </div>
+    //     )}
+    //   </button>
+    // </div>
     <div className="mr-5 -mt-1">
       <button
         onClick={copyToClipboard}
-        className="border-2 border-[#3772FF] text-[#3772FF] text-lg/4 line-heith px-4 py-2 rounded-full"
+        className="border-2 border-[#3772FF] dark:border-indigo-800 text-[#3772FF] dark:text-indigo-300 text-lg/4 line-heith px-4 py-2 rounded-full"
       >
         {copied ? (
-          "کپی شد!"
+          <span className="text-[#3772FF] dark:text-white">کپی شد!</span>
         ) : (
           <div className="flex">
-            <IoIosLink className="-scale-x-[1] ml-2" />
-            کپی کردن لینک صفحه
+            <IoIosLink className="-scale-x-[1] ml-2 text-[#3772FF] dark:text-white" />
+            <span className="text-[#3772FF] dark:text-white">
+              کپی کردن لینک صفحه
+            </span>
           </div>
         )}
       </button>
@@ -51,10 +49,63 @@ const CopyLink = () => {
 };
 
 const CourseDesc = () => {
+  // return (
+  //   <div className="p-6 mb-6 bg-white rounded-lg">
+  //     <h3 className="mb-4 text-xl font-bold text-gray-500">توضیحات دوره</h3>
+  //     <p className="leading-relaxed text-gray-800">
+  //       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
+  //       از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
+  //       سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+  //       متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
+  //       درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با
+  //       نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+  //       خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید
+  //       داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان
+  //       رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+  //       پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+  //       <br />
+  //       <br />
+  //       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
+  //       از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
+  //       سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+  //       متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
+  //       درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با
+  //       نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+  //       خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید
+  //       داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان
+  //       رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+  //       پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+  //       <br />
+  //       <br />
+  //       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
+  //       از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
+  //       سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+  //       متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
+  //       درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با
+  //       نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+  //       خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید
+  //       داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان
+  //       رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
+  //       پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+  //       <br />
+  //       <br />
+  //     </p>
+  //     <div className="flex flex-col justify-start gap-3 mt-5 lg:flex-row ">
+  //       {/* <StarRating rating={4} /> */}
+  //       <div className="mb-6 lg:mb-0">
+  //         <span className="">امتیاز بدید</span>
+  //         <Rate className="mr-4" defaultValue={3} />
+  //       </div>
+  //       <CopyLink />
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="p-6 mb-6 bg-white rounded-lg">
-      <h3 className="mb-4 text-xl font-bold text-gray-500">توضیحات دوره</h3>
-      <p className="leading-relaxed text-gray-800">
+    <div className="p-6 mb-6 bg-white rounded-lg dark:bg-indigo-950">
+      <h3 className="mb-4 text-xl font-bold text-gray-500 dark:text-gray-300">
+        توضیحات دوره
+      </h3>
+      <p className="leading-relaxed text-gray-800 dark:text-gray-200">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
         از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
         سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
@@ -92,10 +143,10 @@ const CourseDesc = () => {
         <br />
         <br />
       </p>
-      <div className="flex flex-col justify-start gap-3 mt-5 lg:flex-row ">
+      <div className="flex flex-col justify-start gap-3 mt-5 lg:flex-row">
         {/* <StarRating rating={4} /> */}
         <div className="mb-6 lg:mb-0">
-          <span className="">امتیاز بدید</span>
+          <span className="text-[#3772FF] dark:text-white">امتیاز بدید</span>
           <Rate className="mr-4" defaultValue={3} />
         </div>
         <CopyLink />
