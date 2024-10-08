a178320674da42734b6180038da5462d051fb73e
ghonche <ataee.ghonche@gmail.com>
2024-10-08 13:24:52 +0330

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        customGray: '#DCDCDC',
-        
-      },
-      
-     
-    },
-
-  },
-  
- 
-  plugins: [ require('daisyui') ],
-}
-
-    extend: {},
+      extend: {},
   },
 
   variants: {
