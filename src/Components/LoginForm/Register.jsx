858c587645cc604bfb3db17bed5b6e2a452a084e
ghonche <ataee.ghonche@gmail.com>
2024-10-09 21:17:57 +0330

    End-of-Responsive

--- src/Components/LoginForm/Register.jsx
+++ src/Components/LoginForm/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Button, Input, Tabs } from "antd";
 import "antd/dist/reset.css";
 import { LoginPanel } from "./LoginPanel";
