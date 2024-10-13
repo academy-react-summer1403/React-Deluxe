ed270d9293fac30da2e9c45e0d32518ea697d88a
ghonche <ataee.ghonche@gmail.com>
2024-10-13 18:13:09 +0330

    Responsive

--- src/Components/Courses/CourseCard.jsx
+++ src/Components/Courses/CourseCard.jsx
@@ -1,11 +1,10 @@
-// import React from "react";
 const CourseCard = ({ title, image, category, level, price, teacher, background }) => {
   return (
-    <div>
+    <div className="flex flex-col w-full max-w-sm mx-auto mb-5">
       {/* دایو اصلی کارت که به آن استایل background می‌دهیم */}
       <div
-        className="relative border rounded-3xl p-4 bg-white flex flex-col h-60 mt-5"
-        style={{ backgroundColor: background }} // اضافه کردن بک‌گراند دلخواه
+        className="relative border rounded-3xl p-4 bg-white flex flex-col h-60 mt-5 shadow-md"
+        style={{ backgroundColor: background }} 
       >
         <div className="absolute top-2 right-2 flex gap-2">
           <p className="text-white bg-purple-500 w-20 text-sm text-center rounded-lg">
@@ -19,16 +18,16 @@ const CourseCard = ({ title, image, category, level, price, teacher, background 
         <img
           src={image}
           alt={title}
-          className="object-cover mb-4 pr-20 rounded w-52 h-52"
+          className="object-cover mb-4 rounded w-full h-40 sm:h-48 md:h-52 lg:h-48 xl:h-52"
         />
       </div>
       
       {/* عنوان دوره */}
-      <h2 className="text-xl font-bold mb-2 text-right mt-2">{title}</h2>
+      <h2 className="text-xl font-bold mb-2 text-right mt-2 dark:text-white">{title}</h2>
 
       {/* اطلاعات مدرس و قیمت */}
-      <div className="flex justify-between">
-        <p className="text-gray-600 text-sm">مدرس: {teacher}</p>
+      <div className="flex flex-col sm:flex-row justify-between">
+        <p className="text-gray-600 text-sm dark:text-white">مدرس: {teacher}</p>
         <p className="text-gray-800 font-semibold">{price} تومان</p>
       </div>
     </div>
