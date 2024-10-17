5e672db81d55d25c09205f3ad07fb006e1493474
ghonche <ataee.ghonche@gmail.com>
2024-10-17 11:26:46 +0330

    darkMode-fixed

--- src/Components/Courses/CoursesComponents/CourseHolder/CourseHolder.jsx
+++ src/Components/Courses/CoursesComponents/CourseHolder/CourseHolder.jsx
@@ -3,14 +3,12 @@
 const CourseHolder = () => {
   return (
     <div className="bg-white text-black dark:text-white p-6 sm:p-8 md:p-10 lg:p-12 xl:p-16 mr-0 sm:mr-6 md:mr-10 lg:mr-14 text-center font-sans relative dark:bg-indigo-950">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 sm:mb-3 md:mb-4">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 sm:mb-3 md:mb-4 lg:mb-12">
         شروع ماجراجویی جدید
       </h1>
-      <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-500 leading-relaxed break-words ">
-        یک شروع قوی برای یادگیری یک مسئله
-      </p>
-      <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-500">
-        جدید میتونه تو پیشرفتت کمک کنه
+      <p className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-500 leading-relaxed break-words dark:text-white">
+        یک شروع قوی برای یادگیری یک مسئله <br />
+        جدید میتونه تو پیشترفت کمک کنه
       </p>
     </div>
   );
